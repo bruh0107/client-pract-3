8f46897df8c0b7420cdec15225a1732806cffb11
Nastya <anastasion0107@gmail.com>
2025-03-20 21:48:58 +0700

    create, move, edit, delete methods in instance

--- vue/js/main.js
+++ vue/js/main.js
@@ -90,5 +90,49 @@ let app = new Vue ({
                 },
             ]
         }
+    },
+    methods: {
+        moveTask(task, newColumnIndex) {
+            const currentColumnIndex = this.columns.findIndex(column => column.tasks.includes(task))
+            if(currentColumnIndex !== -1) {
+                this.columns[currentColumnIndex].tasks = this.columns[currentColumnIndex].tasks.filter(t => t.id !== task.id)
+            }
+            task.status = ['planned', 'in-progress', 'testing', 'completed'][newColumnIndex]
+            task.updateAt = new Date()
+
+            if (newColumnIndex === 3) {
+                task.isOverdue = task.deadline && new Date(task.deadline) < new Date()
+            }
+            this.columns[newColumnIndex].tasks.push(task)
+        },
+        editTask(task) {
+            task.title = prompt('Введите новый заголовок: ', task.title) || task.title
+            task.description = prompt('Введите новое описание: ', task.description) || task.description
+            task.deadline = prompt('Введите новый дэдлайн (YYYY-MM-DD):', task.deadline) || task.deadline
+            task.updateAt = new Date()
+        },
+        deleteTask(task) {
+            const columnIndex = this.columns.findIndex(column => column.tasks.includes(task))
+            if(columnIndex !== -1) {
+                this.columns[columnIndex].tasks = this.columns[columnIndex].tasks.filter(t => t.id !== task.id)
+            }
+        }
     }
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
